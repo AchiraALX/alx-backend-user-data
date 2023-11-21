4a69724d4fd092ef2b57929866f59dd57a83aa8e
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-11-21 23:51:20 +0000

    Complete task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -19,6 +20,9 @@ AUTH_TYPE = os.getenv('AUTH_TYPE', 'auth')
 
 if AUTH_TYPE == 'auth':
     auth = Auth()
+
+if AUTH_TYPE == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Basic authentication module
+"""
+
+
+from .auth import Auth
+
+
+# Basic authentication declaration
+class BasicAuth(Auth):
+    """Performs basic authentication
+    """
+
+    
