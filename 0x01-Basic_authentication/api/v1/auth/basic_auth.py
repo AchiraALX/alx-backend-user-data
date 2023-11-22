2a09b9e6cefce3e108bbc293f3ddd3a7e209033a
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-11-22 00:04:24 +0000

    Complete task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """Basic authentication module
 """
 
+import re
 
 from .auth import Auth
 
@@ -12,4 +13,15 @@ class BasicAuth(Auth):
     """Performs basic authentication
     """
 
-    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts the Base64 part of the authorization
+        """
+
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field = re.fullmatch(pattern, authorization_header.strip())
+
+            if field is not None:
+                return field.group('token')
+
+        return None
